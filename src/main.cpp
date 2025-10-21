052e0e8ab2e8686512ce74e388f7f1427621bd93
ThiagoDeSena <thiagosena316@gmail.com>
2025-10-21 09:26:35 -0300

    feat: Criação do repositorio com o blink

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#define LED_PIN 12
+
+void blinkLED(int intervalo);
+
+void setup() {
+  // Configura o pino do LED como saída
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // Chama a função blinkLED
+  blinkLED(500); // Pisca com intervalo de 500ms
+}
+
+// Função que faz o LED piscar
+void blinkLED(int intervalo) {
+  digitalWrite(LED_PIN, HIGH);  // Acende o LED
+  delay(intervalo);             // Aguarda o intervalo
+  digitalWrite(LED_PIN, LOW);   // Apaga o LED
+  delay(intervalo);             // Aguarda o intervalo
+}
