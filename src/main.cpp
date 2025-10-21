eb16b923eb13c5a25520267ff61874841742527d
ThiagoDeSena <thiagosena316@gmail.com>
2025-10-21 09:55:46 -0300

    Adiciona código ligar led com botão pressionado

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Definindo os pinos
+#define LED 12
+#define BOTAO 7
+
+void controlarLEDComBotao();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED, OUTPUT);
+  pinMode(BOTAO, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Chama função que controla LED baseado no botão
+  controlarLEDComBotao();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void controlarLEDComBotao() {
+  // Lê o estado do botão
+  int estado = digitalRead(BOTAO);
+  
+  // Se botão pressionado (LOW), acende LED
+  if (estado == LOW) {
+    digitalWrite(LED, HIGH);  // Acende LED
+  } else {
+    digitalWrite(LED, LOW);   // Apaga LED
+  }
 }
