d4a53b149b360b7293db943b9bd9e789266936ab
ThiagoDeSena <thiagosena316@gmail.com>
2025-10-22 10:06:54 -0300

    Adiciona código simples para display OLED com funções

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_SSD1306 displayLED;
+
+void escreverNoDisplay(String texto, int linha, int coluna);
+void limparTudo();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  displayLED.begin();
+  displayLED.clearDisplay();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Mensagem centralizada
+  escreverNoDisplay("Bem-vindo!", 1, 1);
+  delay(2000);
+  
+  escreverNoDisplay("Aprendendo", 1, 5);
+  delay(2000);
+  
+  escreverNoDisplay("OLED :)", 1, 10);
+  delay(2000);
+  
+  limparTudo();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Função simplificada para escrever no display
+void escreverNoDisplay(String texto, int coluna, int linha) {
+  displayLED.setTextColor(WHITE); //Define a cor das letras, mesmo sem mudança precisa colocar para mostrar algo no display
+  displayLED.clearDisplay(); //Limpa a mensagem do display
+  displayLED.setTextSize(1);  //Define o tamanho da letra
+  displayLED.setCursor(coluna,linha); // Posição calculada
+  displayLED.print(texto);
+  displayLED.display(); //Confirma a escrita no display
+}
+
+// Função para limpar display
+void limparTudo() {
+  displayLED.clearDisplay();
 }
